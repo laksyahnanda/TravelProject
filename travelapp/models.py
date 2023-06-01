34bbd369b8ef0e3443ce81b14af051fe3622876e
LAKSHMI NANDANA N R <83491502+laksyahnanda@users.noreply.github.com>
2023-06-01 11:06:38 +0530

    Update models.py

--- travelapp/models.py
+++ travelapp/models.py
@@ -5,11 +5,14 @@ class Place(models.Model):
     name=models.CharField(max_length=250)
     img=models.ImageField(upload_to='pics')
     desc=models.TextField()
-#
-# class Destinations(models.Model):
-#     name:models.Charfield(max_length=200)
-#     img:models.ImageField(upload_to='photos')
-#     desc:models.TextField()
+    
+       def __str__(self):
+        return self.name
+
+class Destinations(models.Model):
+    name:models.Charfield(max_length=200)
+    img:models.ImageField(upload_to='photos')
+    desc:models.TextField()
 
     def __str__(self):
         return self.name
